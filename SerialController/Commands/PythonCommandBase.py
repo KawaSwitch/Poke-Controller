1a881e6860226f10dbf9a09c3b77508ed717d746
fu <107758757+futo030@users.noreply.github.com>
2023-01-17 19:37:39 +0900

    6ウィジェット対応のダイアログ関数、テンプレートマッチングのmask機能対応
    6ウィジェット対応のダイアログ関数、テンプレートマッチングのmask機能対応

--- SerialController/Commands/PythonCommandBase.py
+++ SerialController/Commands/PythonCommandBase.py
@@ -151,6 +151,12 @@ class PythonCommand(CommandBase.Command):
         self.message_dialogue = None
         return ret
 
+    def dialogue6widget(self, title: str, dialogue_list: list, need=list):
+        self.message_dialogue = tk.Toplevel()
+        ret = PokeConDialogue(self.message_dialogue, title, dialogue_list, mode=1).ret_value(need)
+        self.message_dialogue = None
+        return ret
+
     # Use time glitch
 
     # Controls the system time and get every-other-day bonus without any punishments
@@ -224,7 +230,24 @@ class PythonCommand(CommandBase.Command):
                 # self.keyPress = None (ここでNoneはNGなはず)
 
 class PokeConDialogue(object):
-    def __init__(self, parent, title: str, message: int | str | list):
+    def __init__(self, parent, title: str, message: int | str | list, mode=0):
+        """
+        pokecon用ダイアログ生成関数(注意:mode=0と1でmessageの取り扱いが大きく異なる。)
+        mode | int: 0のときEntryのみ、1のとき6種類のwidgetに対応
+        title | str: タイトル
+        message | mode=0の場合 : int/str/list: Entryのラベル、mode=1の場合 : list[widget, widget, ...]: widgetごとの設定をリスト化したもの
+        widget | list : widgetごとの設定(ウィジェットの種類によってリストの中身は異なる。以下を参照。)
+        checkbox/entryの場合 : [type, subtitle, init] (例) ["check", "Check(例)", True]、["ENTRY", "Entry(例)", "初期値"]
+        combobox/radiobutton/spinboxの場合 : [type, subtitle, selectlist, init] (例) ["Combo", "Combo(例)", ["hello", "world"], "hello"]、["RADIO", "Radio(例)", ["dog", "cat"],"dog"]、["Spin", "Spin(例)", list(map(str, range(10))), "3"]
+        scaleの場合 : [type, subtitle, min, max, init, digit] (例) ["Scale", "scale(例)", 0, 100, 50.1, 2]
+        type | str: widgetの種類(check/combo/entry/radio/spin/scaleのいずれか。大文字小文字は問わない)
+        subtitle | str : widgetのタイトル
+        init | checkboxの場合bool,scaleの場合int/float,その他str : 初期値
+        selectlist | list : 項目のリスト
+        min/max | int/float : scaleの最小値と最大値
+        digit | int : 有効桁数
+        return : なし
+        """
         self._ls = None
         self.isOK = None
 
@@ -240,9 +263,6 @@ class PokeConDialogue(object):
         self.title_label.grid(column=0, columnspan=2, ipadx='10', ipady='10', row=0, sticky='nsew')
 
         self.dialogue_ls = {}
-        if type(message) is not list:
-            message = [message]
-        n = len(message)
         x = self.message_dialogue.master.winfo_x()
         w = self.message_dialogue.master.winfo_width()
         y = self.message_dialogue.master.winfo_y()
@@ -250,12 +270,11 @@ class PokeConDialogue(object):
         w_ = self.message_dialogue.winfo_width()
         h_ = self.message_dialogue.winfo_height()
         self.message_dialogue.geometry(f"+{int(x+w/2-w_/2)}+{int(y+h/2-h_/2)}")
-        for i in range(n):
-            self.dialogue_ls[message[i]] = tk.StringVar()
-            label = ttk.Label(self.inputs, text=message[i])
-            entry = ttk.Entry(self.inputs, textvariable=self.dialogue_ls[message[i]])
-            label.grid(column=0, row=i, sticky='nsew', padx=3, pady=3)
-            entry.grid(column=1, row=i, sticky='nsew', padx=3, pady=3)
+
+        if mode == 0:
+            self.mode0(message)
+        else:
+            self.mode1(message)
 
         self.inputs.grid(column=0, columnspan=2, ipadx='10', ipady='10', row=1, sticky='nsew')
         self.inputs.grid_anchor('center')
@@ -270,6 +289,90 @@ class PokeConDialogue(object):
         self.result.grid_anchor('center')
         self.main_frame.pack()
         self.message_dialogue.master.wait_window(self.message_dialogue)
+
+    def mode0(self, message):
+        if type(message) is not list:
+            message = [message]
+        n = len(message)
+
+        for i in range(n):
+            self.dialogue_ls[message[i]] = tk.StringVar()
+            label = ttk.Label(self.inputs, text=message[i])
+            entry = ttk.Entry(self.inputs, textvariable=self.dialogue_ls[message[i]])
+            label.grid(column=0, row=i, sticky='nsew', padx=3, pady=3)
+            entry.grid(column=1, row=i, sticky='nsew', padx=3, pady=3)
+
+    def mode1(self, dialogue_list):
+        n = len(dialogue_list)
+        frame = []
+
+        scale_label_list = []   # scaleの値を表示するlabelを格納するリスト
+        scale_index_list = []   # scaleが何番目のwidgetなのかを格納するリスト
+        scale_digit_list = []   # scaleの有効桁数を格納するリスト
+
+        def change_scale_value(event=None):   # scaleのバーを動かしたときにlabelの値を変更するための関数
+            for i, (index, fmt) in enumerate(zip(scale_index_list, scale_digit_list)):
+                if fmt != 0:
+                    val = round(self.dialogue_ls[dialogue_list[index][1]].get(), fmt)
+                    scale_label_list[i]["text"] = "%s" % val
+                    self.dialogue_ls[dialogue_list[index][1]].set(val)
+                else:
+                    scale_label_list[i]["text"] = "%s" % self.dialogue_ls[dialogue_list[index][1]].get()
+
+        for i in range(n):
+            # widgetはすべてframeの中に入れる。scaleの場合、値を示すlabelもフレームの中に入れる。
+            frame.append(ttk.LabelFrame(self.inputs, text=dialogue_list[i][1]))
+
+            # Checkbox
+            if dialogue_list[i][0].casefold() == "check".casefold():
+                self.dialogue_ls[dialogue_list[i][1]] = tk.BooleanVar(value=dialogue_list[i][2])
+                widget = ttk.Checkbutton(frame[i], variable=self.dialogue_ls[dialogue_list[i][1]])
+                widget.grid(column=0, row=0, sticky='nsew', padx=3, pady=3)
+            # Combobox
+            elif dialogue_list[i][0].casefold() == "combo".casefold():
+                self.dialogue_ls[dialogue_list[i][1]] = tk.StringVar(value=dialogue_list[i][3])
+                widget = ttk.Combobox(frame[i], values=dialogue_list[i][2], textvariable=self.dialogue_ls[dialogue_list[i][1]])
+                widget.grid(column=0, row=0, sticky='nsew', padx=3, pady=3)
+                # widget.current(0)
+            # Entry
+            elif dialogue_list[i][0].casefold() == "entry".casefold():
+                self.dialogue_ls[dialogue_list[i][1]] = tk.StringVar(value=dialogue_list[i][2])
+                widget = ttk.Entry(frame[i], textvariable=self.dialogue_ls[dialogue_list[i][1]])
+                widget.grid(column=0, row=0, sticky='nsew', padx=3, pady=3)
+            # Radiobutton
+            elif dialogue_list[i][0].casefold() == "radio".casefold():
+                self.dialogue_ls[dialogue_list[i][1]] = tk.StringVar(value=dialogue_list[i][3])
+                for j, text0 in enumerate(dialogue_list[i][2]):
+                    widget = ttk.Radiobutton(frame[i], text=text0, variable=self.dialogue_ls[dialogue_list[i][1]], value=text0)
+                    widget.grid(column=j, row=0, sticky='nsew', padx=3, pady=3)
+            # Scale
+            elif dialogue_list[i][0].casefold() == "scale".casefold():
+                scale_index_list.append(i)
+                scale_digit_list.append(dialogue_list[i][5])
+                if dialogue_list[i][5] != 0:    # 浮動小数点数
+                    self.dialogue_ls[dialogue_list[i][1]] = tk.DoubleVar(value=dialogue_list[i][4])
+                    scale_label_list.append(tk.Label(frame[i], width=10, text="%s" % round(self.dialogue_ls[dialogue_list[i][1]].get(), dialogue_list[i][5])))
+                else:   # 整数
+                    self.dialogue_ls[dialogue_list[i][1]] = tk.IntVar(value=dialogue_list[i][4])
+                    scale_label_list.append(tk.Label(frame[i], width=10, text="%s" % self.dialogue_ls[dialogue_list[i][1]].get()))
+                widget = ttk.Scale(frame[i], from_=dialogue_list[i][2], to=dialogue_list[i][3], variable=self.dialogue_ls[dialogue_list[i][1]], command=change_scale_value)
+                scale_label_list[-1].grid(column=0, row=0, sticky='nsew', padx=3, pady=3)
+                widget.grid(column=1, row=0, sticky='nsew', padx=3, pady=3)
+            # Spinbox
+            elif dialogue_list[i][0].casefold() == "spin".casefold():
+                self.dialogue_ls[dialogue_list[i][1]] = tk.StringVar(value=dialogue_list[i][3])
+                widget = ttk.Spinbox(frame[i], values = dialogue_list[i][2], textvariable=self.dialogue_ls[dialogue_list[i][1]])
+                widget.grid(column=0, row=0, sticky='nsew', padx=3, pady=3)
+
+            frame[i].grid(column=0, row=i, sticky='nsew', padx=3, pady=3)
+
+        # widgetのサイズをフレームのサイズに合わせる
+        for i in range(n):
+            if dialogue_list[i][0].casefold() == "scale".casefold():
+                frame[i].grid_columnconfigure(0, weight=1)
+                frame[i].grid_columnconfigure(1, weight=3)
+            else:
+                frame[i].grid_columnconfigure(0, weight=1)
 
     def ret_value(self, need):
         if self.isOK:
@@ -301,12 +404,9 @@ TEMPLATE_PATH = "./Template/"
 def _get_template_filespec(template_path: str) -> str:
     """
     テンプレート画像ファイルのパスを取得する。
-
     入力が絶対パスの場合は、`TEMPLATE_PATH`につなげずに返す。
-
     Args:
         template_path (str): 画像パス
-
     Returns:
         str: _description_
     """
@@ -339,18 +439,26 @@ class ImageProcPythonCommand(PythonCommand):
     # 現在のスクリーンショットと指定した画像のテンプレートマッチングを行います
     # 色の違いを考慮しないのであればパフォーマンスの点からuse_grayをTrueにしてグレースケール画像を使うことを推奨します
     def isContainTemplate(self, template_path, threshold=0.7, use_gray=True,
-                          show_value=False, show_position=True, show_only_true_rect=True, ms=2000, crop=[]):
+                          show_value=False, show_position=True, show_only_true_rect=True, ms=2000, crop=[], mask_path=None):
         src = self.camera.readFrame()
         src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) if use_gray else src
         
         if len(crop) == 4:
             src = src[crop[1]: crop[3], crop[0]: crop[2]]
-        
+
         template = cv2.imread(_get_template_filespec(template_path), cv2.IMREAD_GRAYSCALE if use_gray else cv2.IMREAD_COLOR)
+
+        # mask用画像読み込み
+        if mask_path == None:
+            mask = None
+            method = cv2.TM_CCOEFF_NORMED
+        else:
+            mask = cv2.imread(_get_template_filespec(mask_path), 0)
+            method = cv2.TM_CCORR_NORMED
+
         w, h = template.shape[1], template.shape[0]
 
-        method = cv2.TM_CCOEFF_NORMED
-        res = cv2.matchTemplate(src, template, method)
+        res = cv2.matchTemplate(src, template, method, mask)
         _, max_val, _, max_loc = cv2.minMaxLoc(res)
 
         if show_value:
